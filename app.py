3ec0df171fa89f7a198a14e338d8bd111ef01d84
devnydv <80939011+devnydv@users.noreply.github.com>
2024-06-16 23:57:21 +0530

    Update app.py
    Static new satting

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, request
 from api import filehandle, getpost
 from savedata import hit
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 
 
 
