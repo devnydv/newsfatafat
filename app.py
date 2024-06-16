1ae43a33e6def3f73fd7072cbe9fc57457f34796
devnydv <80939011+devnydv@users.noreply.github.com>
2024-06-17 00:05:09 +0530

    Update app.py
    Route for robots .txt

--- app.py
+++ app.py
@@ -84,6 +84,10 @@ def saveapi():
 def sour():
     return render_template("credit.html")
 
+@app.route('/robots.txt')
+def robots_txt():
+    return send_from_directory(app.root_path, 'robots.txt')
+
 if __name__ == "__main__":
     app.run(debug=True)
 
