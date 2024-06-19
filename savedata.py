27cea7e472c356c3c993f1b637156b534120ac6a
devnydv <80939011+devnydv@users.noreply.github.com>
2024-06-19 22:33:24 +0530

    Update savedata.py
    added translation

--- savedata.py
+++ savedata.py
@@ -1,7 +1,24 @@
 import requests 
-import json
+from easygoogletranslate import EasyGoogleTranslate
+
+# CODE TO FETCH only one DATA FROMAPI, TRANSLATE it AND SENT TO DB
+
+def tr(item): 
+    translator = EasyGoogleTranslate(
+        source_language='en',
+        target_language='hi',
+        timeout=10
+    )
+    result = translator.translate(item)
+    return result
+# Output: Dies ist ein Beispiel.
+
+
+
+#import json
 
 cat = ["all", "science", "sports", "entertainment", "technology"]
+#cat = ["entertainment"]
 def hit():
     for category in cat:
         savingdata(category)
@@ -24,7 +41,6 @@ def savingdata(category):
     dburl = f'https://filmyapp-e1005.firebaseio.com/news/{category}/data.json'
     dbres = requests.get(dburl)
     datafromdb= dbres.json()
-
     
     originaldDtaSize = len(datafromdb)
     lesssize = len(datafromdb) -1 
@@ -33,17 +49,16 @@ def savingdata(category):
     #print(data_title)
     #if title are save do not add data and if not same save data to db
     if main_title== data_title:
-        return "data save krne ki koi jarurat nhi hai"
+        print("no need to edit...")
     else:
-        for x in datatodb:
-            
-            x["id"]= originaldDtaSize
-            #print(x["id"])
+        for x in range(2):
+            datatodb[x]["id"] =originaldDtaSize
+            transtitle = datatodb[x]["title"]
+            transDesc = datatodb[x]['content']
+            datatodb[x]["title"] = tr(transtitle)
+            datatodb[x]["content"] = tr(transDesc)
             saveurl = f'https://filmyapp-e1005.firebaseio.com/news/{category}/data/{originaldDtaSize}.json'
-            saved = requests.put(saveurl, json=x)
+            saved = requests.put(saveurl, json = datatodb[x])
             originaldDtaSize =originaldDtaSize + 1
             #print(originaldDtaSize)
-        
-        
-            
-            
+            print (datatodb[x]["title"])
